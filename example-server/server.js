1281c471ddc18169f222c86d7dc5ba2deae717e0
MOSUMA <65025326+TDVAppLab@users.noreply.github.com>
2023-04-19 22:13:05 +0900

    step01

--- example-server/server.js
+++ example-server/server.js
@@ -2,6 +2,28 @@
  * Basic implementation of a history server.
  */
 
+/*
+//sqlite
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./mock.db", sqlite3.OPEN_READWRITE,(error)=>{
+    if (error) return console.error(error.message);
+
+//    console.log("connection suessfull");
+});
+
+//db.run( "CREATE TABLE isspos(timestamp INTEGER ,altitude REAL)" );
+//db.run( "DELETE FROM isspos ");
+//db.run( "insert into isspos values(1681902382000, 421.96029468591)" );
+//db.run( "insert into isspos values(1681904558000, 413.73966823439)" );
+//db.run( "insert into isspos values(1681909689000, 415.6573746748)" );
+
+
+db.close((err) => {
+    if(err) return console.error(err.message);
+})
+//-------------------------------------------------------------------
+*/
 var StaticServer = require('./static-server');
 var app = require('express')();
 
